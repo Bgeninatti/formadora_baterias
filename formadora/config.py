e0d9c4fe4a1f6d5732614bead7dad95bef02c29c
Bruno Geninatti <brunogeninatti@gmail.com>
2021-01-20 21:39:17 +0000

    fix config path

--- formadora/config.py
+++ formadora/config.py
@@ -1,11 +1,7 @@
 import configparser
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def load_config(base_dir=BASE_DIR):
-    config_file = os.path.join(base_dir, 'config.ini')
+def load_config(config_file):
     config = configparser.ConfigParser()
     config.read(config_file)
     return config
