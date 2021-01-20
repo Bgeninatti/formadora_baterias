b6ab3b62b635ffb0e0bbda9be036d8453a8d7234
Bruno Geninatti <brunogeninatti@gmail.com>
2021-01-20 21:39:47 +0000

    add telegram bot command

--- formadora/main.py
+++ formadora/main.py
@@ -1,11 +1,17 @@
-import sys
 import argparse
-from .client import TKL688
-from .config import get_config
+import sys
+
 from . import __version__
+from .client import TKL688
+from .config import load_config
+from .telegram_bot import TKLBot
+
+
+HELP_EPILOG = "Herramienta de monitoreo para formadora de baterias"
 
 
 def go():
+    parser = argparse.ArgumentParser(prog='formadora', epilog=HELP_EPILOG)
     parser.add_argument(
         '--config-file', default='config.ini')
     parser.add_argument(
@@ -19,13 +25,18 @@ def go():
         default=False,
         help="Print version and exit")
     args = parser.parse_args()
-    config = get_config(args.config_file)
+    config = load_config(args.config_file)
 
     if args.version:
         sys.stdout.write(f"Formadora {__version__}")
         return 0
 
-    client = TKL688(config['RASPI']['ip'])
-    
+    client = TKL688(ip=config['DEFAULT']['RaspiIp'])
+    if args.run_bot:
+        bot = TKLBot(
+            client=client,
+            bot_key=config['DEFAULT']['TelegramToken'],
+        )
+        bot.run()
 
     print(client)
